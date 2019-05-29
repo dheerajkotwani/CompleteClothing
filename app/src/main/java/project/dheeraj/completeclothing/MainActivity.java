d515af7ca662d80c4446862cc53c2694f6ccdfe8
dheerajkotwani <dheeraj.kotwani41@gmail.com>
2019-05-29 23:38:35 +0530

    Initial commit

---
+++ app/src/main/java/project/dheeraj/completeclothing/MainActivity.java
@@ -0,0 +1,29 @@
+package project.dheeraj.completeclothing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.button_started);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
